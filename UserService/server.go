818aff83aa69a04c87319d69ca30c95fcc563013
OpenClade1 <openclade@mail.ru>
2022-09-01 13:10:45 +0600

    feat (UserService): add user struct

---
+++ UserService/server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+
+func main() {
+	version := "1.0"
+	fmt.Println("Starting version " + version)
+	
+	// Initialize the dispatcher
+
+}
